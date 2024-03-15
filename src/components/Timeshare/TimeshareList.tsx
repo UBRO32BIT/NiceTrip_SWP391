8a921e2d9bc7a67643b223548913e40deb54a558
bwfnguyenvu <bwfnguyenvu@gmail.com>
2024-03-16 04:56:34 +0700

    update v2

--- src/components/Timeshare/TimeshareList.tsx
+++ src/components/Timeshare/TimeshareList.tsx
@@ -127,8 +127,8 @@ export default function TimeshareList() {
             </Grid>
             <Grid container spacing={2} sx={{flexGrow: 1}}>
                 {myPosts.length > 0 && myPosts.map((post: any) => {
+                    if (!post?.deleted) {
                 return (<Grid xs={12} md={6} lg={4} >
-                    {post?.deleted === false ? (
                         <Card sx={{}}>
                         <div>
                             <Typography level="title-lg" noWrap>{post?.resortId?.name}</Typography>
@@ -252,56 +252,13 @@ export default function TimeshareList() {
                                 Manage
                             </Button>
                         </CardContent>
-                    </Card>
-                    ) : (
-                        ''
-                    )}
-                   
-                </Grid>)
-            })}
+                        </Card>
+                        </Grid>
+                        );
+                    }
+                    return null; 
+                })}
             </Grid>
-
-
-            {/* <Card sx={{ width: 320 }}>
-                <div>
-                    <Typography level="title-lg">Yosemite National Park</Typography>
-                    <Typography level="body-sm">April 24 to May 02, 2021</Typography>
-                    <IconButton
-                        aria-label="bookmark Bahamas Islands"
-                        variant="plain"
-                        color="neutral"
-                        size="sm"
-                        sx={{ position: 'absolute', top: '0.875rem', right: '0.5rem' }}
-                    >
-                        <BookmarkAdd />
-                    </IconButton>
-                </div>
-                <AspectRatio minHeight="120px" maxHeight="200px">
-                    <img
-                        src="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286"
-                        srcSet="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=286&dpr=2 2x"
-                        loading="lazy"
-                        alt=""
-                    />
-                </AspectRatio>
-                <CardContent orientation="horizontal">
-                    <div>
-                        <Typography level="body-xs">Total price:</Typography>
-                        <Typography fontSize="lg" fontWeight="lg">
-                            $2,900
-                        </Typography>
-                    </div>
-                    <Button
-                        variant="solid"
-                        size="md"
-                        color="primary"
-                        aria-label="Explore Bahamas Islands"
-                        sx={{ ml: 'auto', alignSelf: 'center', fontWeight: 600 }}
-                    >
-                        Explore
-                    </Button>
-                </CardContent>
-            </Card> */}
         </Grid>
 
     );
